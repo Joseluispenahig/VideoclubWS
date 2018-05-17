1e265cb87bb5accd865005137c09bf81ff4ad51d
alfonso <alfonso@alfonso-portatil>
2018-05-17 15:14:03 +0200

    [ABC] VideoclubWS
    Cambio directorio PeliculaRes y cambio en Pelicula

--- serviciovideoclub/videoclubwebservice/Pelicula.java
+++ serviciovideoclub/videoclubwebservice/Pelicula.java
@@ -4,13 +4,16 @@ public class Pelicula {
 	private String nombre;
 	private String genero;
 	private String cantidad;
+	private double preciopordia;
 	
-	public Pelicula(int id, String nombre, String genero, int cantidad) {
+	public Pelicula(int id, String nombre, String genero, int cantidad, double preciopordia) {
 		//super();
 		this.id = id;
 		this.nombre = nombre;
 		this.genero = genero;
 		this.cantidad = cantidad;
+		this.preciopordia = preciopordia;
+		
 	}
 		
 	public int getId() {
@@ -29,6 +32,10 @@ public class Pelicula {
 		return this.cantidad;
 	}
 	
+	public double getPreciopordia() {
+		return this.preciopordia;
+	}
+	
 	public void setId(int id) {
 		this.id = id;
 	}
@@ -43,5 +50,9 @@ public class Pelicula {
 	
 	public void setCantidad(String cantidad) {
 		this.cantidad = cantidad;
+	}
+	
+	public void setPreciopordia(double preciopordia) {
+		this.preciopordia = preciopordia;
 	}
 }
